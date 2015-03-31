e39dbb9f6edf913e34feea974086409ab62aac52
LoiWu <superloiwu@gmail.com>
2015-03-31 17:21:20 +0800

    ARCSocketIM Build 1.0.1004 - weak property

---
+++ SocketIM/ARCSocketIM/ARCSocketIM/SomeObject.h
@@ -0,0 +1,15 @@
+//
+//  SomeObject.h
+//  ARCSocketIM
+//
+//  Created by loi on 3/31/15.
+//  Copyright (c) 2015 GWrabbit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SomeObject : NSObject
+
+@property (nonatomic, weak) id obj;
+
+@end
---
+++ SocketIM/ARCSocketIM/ARCSocketIM/SomeObject.m
@@ -0,0 +1,13 @@
+//
+//  SomeObject.m
+//  ARCSocketIM
+//
+//  Created by loi on 3/31/15.
+//  Copyright (c) 2015 GWrabbit. All rights reserved.
+//
+
+#import "SomeObject.h"
+
+@implementation SomeObject
+
+@end
